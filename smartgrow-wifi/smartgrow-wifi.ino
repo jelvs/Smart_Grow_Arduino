bb15f93f5f3667005c204f87872ad8a1b4629ce9
aanciaes <aanciaes.miguel@gmail.com>
2019-05-20 16:38:45 +0100

    Added temperature sensor example and wifi serial comm example

---
+++ smartgrow-wifi/smartgrow-wifi.ino
@@ -0,0 +1,24 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(12, 14); // RX, TX
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.println("ESP Started!");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(4800);
+  //mySerial.println("I am esp");
+}
+
+void loop() { // run over and over
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+}
